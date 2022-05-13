ac33626aa37e30dc7f1b3c240106a55027e84f5b
NENG0910 <85937816+NENG0910@users.noreply.github.com>
2022-05-13 11:45:22 +0800

    router setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} exact />
+        <Route path="/about" element={<About />} exact />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
