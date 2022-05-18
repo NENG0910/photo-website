6a196f2c61a1ba25af26d1497943173b6bed5a48
NENG0910 <85937816+NENG0910@users.noreply.github.com>
2022-05-18 15:49:43 +0800

    more picture by myself

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,11 +5,14 @@ import Picture from "../components/Picture";
 const Homepage = () => {
   const [input, setInput] = useState("");
   let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "563492ad6f917000010000012f336030dd0147558acab6f4d0459ee9";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}e&page=1&per_page=15`;
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}e&page=1&per_page=15`;
 
   const search = async (url) => {
+    setPage(2);
     const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
@@ -21,15 +24,42 @@ const Homepage = () => {
     setData(parsedData.photos);
   };
 
+  const morePicture = async () => {
+    let newURL;
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}e&page=${page}&per_page=15`;
+    }
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setData(data.concat(parsedData.photos));
+  };
+
   useEffect(() => {
     search(initialURL);
   }, []);
+
+  useEffect(() => {
+    if (currentSearch === "") {
+      search(initialURL);
+    } else {
+      search(searchURL);
+    }
+  }, [currentSearch]); //React Hook useEffect has missing dependencies
 
   return (
     <div style={{ minHeight: "100vh" }}>
       <Search
         search={() => {
-          search(searchURL);
+          setCurrentSearch(input);
         }}
         setInput={setInput}
       />
@@ -38,6 +68,9 @@ const Homepage = () => {
           data.map((d) => {
             return <Picture data={d} key={d.id} />;
           })}
+      </div>
+      <div className="morepicture">
+        <button onClick={morePicture}>More Picture</button>
       </div>
     </div>
   );
